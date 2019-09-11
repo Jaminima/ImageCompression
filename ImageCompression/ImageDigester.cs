c9df250676a5680ddab48cba50ef9ee89e10a0cf
Oscar Davies <oscar.william.davies@outlook.com>
2019-09-11 18:14:09 +0100

    Remove Binary Compression

--- ImageCompression/ImageDigester.cs
+++ ImageCompression/ImageDigester.cs
@@ -23,23 +23,6 @@ namespace ImageCompression
             return ImageData;
         }
 
-        public static BinaryCompressed LoadBinary(string FilePath)
-        {
-            BinaryCompressed ImageData = new BinaryCompressed();
-            BinaryReader Reader = new BinaryReader(File.Open(FilePath, FileMode.Open));
-            int Width = Reader.ReadInt32(), Height = Reader.ReadInt32();
-            ImageData.PixelTreePaths = new List<bool>[Width, Height, 3];
-            ImageData.BaseColor = new int[] { Reader.ReadInt32(), Reader.ReadInt32(), Reader.ReadInt32() };
-            for (int x = 0, y = 0; x < Width && y < Height; x++)
-            {
-                ImageData.PixelTreePaths[x, y, 0] = LoadPath(Reader);
-                ImageData.PixelTreePaths[x, y, 1] = LoadPath(Reader);
-                ImageData.PixelTreePaths[x, y, 2] = LoadPath(Reader);
-                if (x + 1 == Width) { x = -1; y++; }
-            }
-            return ImageData;
-        }
-
         static List<bool> LoadPath(BinaryReader Reader)
         {
             List<bool> Path = new List<bool> { };
@@ -58,21 +41,6 @@ namespace ImageCompression
                 if (x + 1 == ImageRaw.Width) { x = -1; y++; }
             }
             ImageRaw.Save(FilePath);
-        }
-
-        public static void SaveBinary(string FilePath, BinaryCompressed ImageData)
-        {
-            int Width = ImageData.PixelTreePaths.GetLength(0), Height = ImageData.PixelTreePaths.GetLength(1);
-            BinaryWriter Writer = new BinaryWriter(File.Open(FilePath, FileMode.Create));
-            Writer.Write(Width); Writer.Write(Height);
-            Writer.Write(ImageData.BaseColor[0]); Writer.Write(ImageData.BaseColor[1]); Writer.Write(ImageData.BaseColor[2]);
-            for (int x = 0, y = 0; x < Width && y < Height; x++)
-            {
-                SaveBinary(Writer, ImageData.PixelTreePaths[x, y, 0]);
-                SaveBinary(Writer, ImageData.PixelTreePaths[x, y, 1]);
-                SaveBinary(Writer, ImageData.PixelTreePaths[x, y, 2]);
-                if (x + 1 == Width) { x = -1; y++; }
-            }
         }
 
         static void SaveBinary(BinaryWriter Writer, List<bool> Path)
--- ImageCompression/Program.cs
+++ ImageCompression/Program.cs
@@ -13,14 +13,10 @@ namespace ImageCompression
             int[,,] ImageData = ImageDigester.LoadImage("./Image.png");
             int[,,] NewImageData = ImageManipulator.DropResolution(ImageData, 1f, 1f);
 
-            List<int[]> Colors = ImageManipulator.GetTopColors(NewImageData,10,5);
+            List<int[]> Colors = ImageManipulator.GetTopColors(NewImageData,12,8);
             NewImageData = ImageManipulator.UseOnlyColors(NewImageData, Colors);
 
             ImageDigester.SaveImage("NewImage.png", NewImageData);
-
-            //BinaryCompressed B = BinaryCompression.Compress(NewImageData, 1, 0);
-            //ImageDigester.SaveImage("./NewImage.png", BinaryDecompression.ToIntImage(B));
-            //ImageDigester.SaveBinary("./NewImage.bin", B);
         }
     }
 }
